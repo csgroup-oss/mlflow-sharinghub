69905bc2a3f00c0e4c4f9f96ec06cea6bcfff7e7
Clément GUICHARD <clement.guichard@csgroup.eu>
2024-05-15 16:12:13 +0200

    feat: change SharingHub auth page btn for a simple logout btn

--- src/mlflow_sharinghub/patch.py
+++ src/mlflow_sharinghub/patch.py
@@ -9,16 +9,16 @@ window.onload = () => {{
     const header_links = document.getElementsByClassName("header-links")[0];
     const github_link = header_links.children[1];
     const new_link = github_link.cloneNode();
-    new_link.href = "{auth_href}";
-    new_link.innerHTML = '<div style="color:#e7f1fb;"><span>SharingHub</span></div>';
-    header_links.insertBefore(new_link, github_link);
+    new_link.href = "{logout_href}";
+    new_link.innerHTML = '<div style="color:#e7f1fb;"><span>Logout</span></div>';
+    header_links.appendChild(new_link);
 }};
 """
 
 
 def alter_main_js(resp: Response) -> None:
     """Fix the main js file of the built frontend."""
-    inject_js = ";\n" + _INJECT_JS.format(auth_href=url_for("auth.index"))
+    inject_js = ";\n" + _INJECT_JS.format(logout_href=url_for("auth.logout"))
 
     resp.direct_passthrough = False
     resp.set_data(resp.get_data() + inject_js.encode())
