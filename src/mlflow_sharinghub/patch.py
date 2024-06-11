86a4b687b618d2080e2416f5fa521dadbe8c7cbb
Clément GUICHARD <clement.guichard@csgroup.eu>
2024-06-11 14:22:26 +0200

    feat: add nav divider, and a home button for project views

--- src/mlflow_sharinghub/patch.py
+++ src/mlflow_sharinghub/patch.py
@@ -18,23 +18,41 @@
 
 from flask import Response
 
-from mlflow_sharinghub._internal.server import url_for
+from mlflow_sharinghub._internal.server import get_project_path, url_for
 
 _INJECT_JS = """
 window.onload = () => {{
-    const header_links = document.getElementsByClassName("header-links")[0];
-    const github_link = header_links.children[1];
-    const new_link = github_link.cloneNode();
-    new_link.href = "{logout_href}";
-    new_link.innerHTML = '<div style="color:#e7f1fb;"><span>Logout</span></div>';
-    header_links.appendChild(new_link);
+    const headerLinks = document.getElementsByClassName("header-links")[0];
+    const githubLink = headerLinks.children[1];
+
+    const divider = document.createElement("div");
+    divider.className = githubLink.className;
+    divider.style.borderRight = "solid 1px #e7f1fb"
+    headerLinks.appendChild(divider);
+
+    const logoutLink = githubLink.cloneNode();
+    logoutLink.href = "{logout_href}";
+    logoutLink.innerHTML = `<div style="color: #e7f1fb;"><span>Logout</span></div>`;
+    headerLinks.appendChild(logoutLink);
+
+    const homeHref = "{home_href}";
+    if (!!homeHref.length) {{
+        const homeLink = githubLink.cloneNode();
+        homeLink.href = homeHref;
+        homeLink.innerHTML = `<div style="color: #e7f1fb;"><span>Home</span></div>`;
+        headerLinks.appendChild(homeLink);
+    }}
 }};
 """
 
 
 def alter_main_js(resp: Response) -> None:
     """Fix the main js file of the built frontend."""
-    inject_js = ";\n" + _INJECT_JS.format(logout_href=url_for("auth.logout"))
+    project_path = get_project_path()
+    inject_js = ";\n" + _INJECT_JS.format(
+        home_href="/" if project_path else "",
+        logout_href=url_for("auth.logout"),
+    )
 
     resp.direct_passthrough = False
     resp.set_data(resp.get_data() + inject_js.encode())
