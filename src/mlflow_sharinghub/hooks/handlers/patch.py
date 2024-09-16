063012b7eab0f2808555450df6fde04a1bb918b6
Clément GUICHARD <clement.guichard@csgroup.eu>
2024-09-16 16:35:09 +0200

    feat(ui): add refresh button if iframe

--- src/mlflow_sharinghub/hooks/handlers/patch.py
+++ src/mlflow_sharinghub/hooks/handlers/patch.py
@@ -53,6 +53,7 @@ waitForElm('header').then((header) => {{
     const headerLeftLinks = header.children[1];
     const headerRightLinks = header.children[header.children.length - 1];
 
+    const labelStyle = "transform:translateY(2px);"
     const btnStyle = "font-size:16px;transform:translateY(-8px);";
     const tagStyle = "font-size:12px;height:23px;" +
                      "padding-right:5px;padding-left:5px;" +
@@ -102,9 +103,15 @@ waitForElm('header').then((header) => {{
             logoutLink.style = btnStyle;
             headerRightLinks.appendChild(logoutLink);
         }}
+    }} else {{
+        const refreshLink = document.createElement("a");
+        refreshLink.href = "";
+        refreshLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="20px" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false" class=""><path fill="currentColor" d="M8 2.5a5.48 5.48 0 0 1 3.817 1.54l.009.009.5.451H11V6h4V2h-1.5v1.539l-.651-.588A7 7 0 0 0 1 8h1.5A5.5 5.5 0 0 1 8 2.5ZM1 10h4v1.5H3.674l.5.451.01.01A5.5 5.5 0 0 0 13.5 8h1.499a7 7 0 0 1-11.849 5.048L2.5 12.46V14H1v-4Z"></path></svg>`;
+        refreshLink.style = labelStyle;
+        headerRightLinks.appendChild(refreshLink);
     }}
 }});
-"""
+"""  # noqa: E501
 
 
 def alter_main_js(resp: Response) -> None:
