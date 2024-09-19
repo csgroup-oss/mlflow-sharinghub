4529d99ad086007edbfcc9f617ca594e6abc30cb
Clément GUICHARD <clement.guichard@csgroup.eu>
2024-09-19 16:19:51 +0200

    feat(ui): add open button if iframe

--- src/mlflow_sharinghub/hooks/handlers/patch.py
+++ src/mlflow_sharinghub/hooks/handlers/patch.py
@@ -109,6 +109,13 @@ waitForElm('header').then((header) => {{
         refreshLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="20px" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false" class=""><path fill="currentColor" d="M8 2.5a5.48 5.48 0 0 1 3.817 1.54l.009.009.5.451H11V6h4V2h-1.5v1.539l-.651-.588A7 7 0 0 0 1 8h1.5A5.5 5.5 0 0 1 8 2.5ZM1 10h4v1.5H3.674l.5.451.01.01A5.5 5.5 0 0 0 13.5 8h1.499a7 7 0 0 1-11.849 5.048L2.5 12.46V14H1v-4Z"></path></svg>`;
         refreshLink.style = labelStyle;
         headerRightLinks.appendChild(refreshLink);
+
+        const openLink = document.createElement("a");
+        openLink.href = "";
+        openLink.target = "_blank";
+        openLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="20px" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false" class=""><path fill="currentColor" d="M10 1h5v5h-1.5V3.56L8.53 8.53 7.47 7.47l4.97-4.97H10V1Z"></path><path fill="currentColor" d="M1 2.75A.75.75 0 0 1 1.75 2H8v1.5H2.5v10h10V8H14v6.25a.75.75 0 0 1-.75.75H1.75a.75.75 0 0 1-.75-.75V2.75Z"></path></svg>`;
+        openLink.style = labelStyle;
+        headerRightLinks.appendChild(openLink);
     }}
 }});
 """  # noqa: E501
